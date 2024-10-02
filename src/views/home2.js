991dbe1a949255477ebc95def6b87d00fcc2fe2e
apivis <88725360+ulfapivis@users.noreply.github.com>
2024-10-02 19:40:10 +0200

    Commit from TeleportHQ

--- src/views/home2.js
+++ src/views/home2.js
@@ -131,17 +131,14 @@ const Home2 = (props) => {
         await Clerk.load();
 
         Clerk.addListener('auth:change', (event) => {
-          const protectedContent = document.getElementById('protected-content');
-          if (event.isSignedIn) {
-            protectedContent.style.display = 'block';
-          } else {
-            protectedContent.style.display = 'none';
-          }
-        });
-      } catch (error) {
-        console.error('ClerkJS load error:', error);
-      }
-    });
+        const protectedContent = document.getElementById('protected-content');
+        if (event.isSignedIn) {
+          protectedContent.style.display = 'block';
+        } else {
+          protectedContent.style.display = 'none';
+        }
+      });
+    };
   </script>`}
           ></Script>
         </div>
